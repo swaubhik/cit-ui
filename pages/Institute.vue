193cbbe3222fe99a36265844303fc9637cc5c3eb
Swaubhik Chakraborty <swaubhik@users.noreply.github.com>
2023-07-19 04:16:25 +0530

    Update Institute.vue

--- pages/Institute.vue
+++ pages/Institute.vue
@@ -1,7 +1,61 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>Page: foo</div>
+  <section class="px-10">
+    <!-- about the institute -->
+    <div class="container mx-auto">
+      <div class="flex flex-wrap">
+        <div class="w-full md:w-1/2 p-6">
+          <img
+            src="https://source.unsplash.com/random/500x300/?college"
+            alt=""
+            class="w-full h-full object-cover object-center rounded-md shadow-md"
+          />
+        </div>
+        <div class="w-full md:w-1/2 p-6 flex flex-col justify-center">
+          <h3
+            class="text-4xl text-center text-gray-800 font-bold leading-none mb-5"
+          >
+            The Institute
+          </h3>
+          <div class="text-gray-600 text-justify leading-6 space-y-2">
+            <p>
+              Central Institute of Technology (CIT), Kokrajhar is situated in
+              Kokrajhar District of Bodoland Territorial Council (BTC) in Assam.
+              CIT has been established for the basic objective of fulfilling the
+              aspirations of the Bodo People relating to their cultural
+              identity, language, education and overall economic development of
+              the region and to impart Bodo youths with requisite technological
+              and vocational training to produce the required manpower to give
+              the impetus to economic growth of this area and to integrate the
+              Bodo People into the mainstream of Technical and Vocational
+              Education. It is a Centrally Funded Institute under the Ministry
+              of Human Resource Development, Government of India.
+            </p>
+            <p>
+              The Institute was established on the 19th of December 2006. The
+              genesis of this Institute was the memorandum of Settlement on
+              Bodoland Territorial Council (BTC) signed between the Assam
+              Government, the Union Government and the Bodo Liberation Tigers,
+              on February 10, 2003, in New Delhi. The Institute is an autonomous
+              body registered under the Societies Registration Act., 1860 and
+              functions under a Board of Governors (BOG).
+            </p>
+            <p>
+              CIT is mandated to impart Technical and Vocational Education such
+              as Information Technology, Bio-Technology, Food Processing, Rural
+              Industries, Business Management, etc. as part of the concerted
+              efforts being made by the Government of India and the Government
+              of Assam to fulfill the aspirations of the Bodo people. It is thus
+              envisioned to acquire a unique place in the field of technical
+              education in the country through its modular and innovative
+              academic programmes.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
