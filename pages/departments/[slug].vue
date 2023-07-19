3aeb8788c1b8dbc5a976d5573316ca4b60152ce9
swaubhik <p22cse1008@cit.ac.in>
2023-07-19 15:38:28 +0530

    Create [slug].vue

---
+++ pages/departments/[slug].vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="px-10">
+    <div class="flex">
+      <div class="flex flex-col prose">
+        <ContentDoc />
+      </div>
+      <NuxtImg
+        class="bg-black"
+        :src="department.background.url"
+        :alt="department.background.alt"
+        width="100%"
+      />
+    </div>
+  </article>
+</template>
+<script lang="ts" setup>
+definePageMeta({
+  layout: "department",
+});
+const route = useRoute();
+
+const department = await queryContent("departments")
+  .where({ slug: route.params.slug })
+  .findOne();
+</script>
