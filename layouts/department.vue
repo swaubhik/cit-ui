0818c4150745cdd9288eded177e02e1b39b65862
swaubhik <p22cse1008@cit.ac.in>
2023-07-19 15:36:35 +0530

    Create department.vue
    added new custom layout for departments

---
+++ layouts/department.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="">
+    <Header />
+    <div class="max-w-full mx-auto min-h-screen">
+      <slot />
+    </div>
+    <Footer />
+  </main>
+</template>
+<script setup lang="ts">
+const route = useRoute();
+
+const department = await queryContent("departments")
+  .where({ slug: route.params.slug })
+  .findOne();
+</script>
