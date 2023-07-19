8b643aa5d5df4af225cbf33aecacb322de5f6eaf
swaubhik <p22cse1008@cit.ac.in>
2023-07-19 15:38:30 +0530

    Create index.vue

---
+++ pages/departments/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: "default",
+});
+const { data: departments } = await useAsyncData("departments", () => {
+  return queryContent("departments").find();
+});
+</script>
+
+<template>
+  <NuxtLink
+    v-for="(department, index) in departments"
+    :key="index"
+    :to="`/departments/${department.slug}`"
+    >{{ department.title }}</NuxtLink
+  >
+</template>
