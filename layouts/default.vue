6acbdcc1f5436089ca6ed1984ca707cf4c91d925
swaubhik <p22cse1008@cit.ac.in>
2023-07-19 15:36:16 +0530

    Update default.vue
    Updated default style

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,9 @@
         <div class="hero-overlay bg-opacity-70"></div>
         <div class="hero-content text-center text-neutral-content">
           <div class="max-w-4xl flex items-center justify-center flex-col">
-            <h1 class="text-7xl font-bold text-white mb-5">{{ pageTile }}</h1>
+            <h1 class="text-7xl font-bold text-white mb-5 capitalize">
+              {{ pageTile }}
+            </h1>
             <p class="mb-5 text-5xl font-bold w-max">
               Central Institue of Technology Kokrajhar
             </p>
